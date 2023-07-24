2ef902fe8f508242d6a442cfabdc2da9ca8b2aa1
Andrés <dirox7@gmail.com>
2023-07-24 08:13:03 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,19 +1,32 @@
 <?php
-function obtenerConexion() {
-    $host = "127.0.0.1";
-    $port = 3306;
-    $socket = "";
-    $user = "root";
-    $password = "";
-    $dbname = "ProyectoPhp"; // El nombre de la base de datos es con letras mayúsculas
+class Conexion {
+    private $host = "127.0.0.1";
+    private $port = 3306;
+    private $socket = "";
+    private $user = "root";
+    private $password = "";
+    private $dbname = "ProyectoPhp"; // El nombre de la base de datos es con letras mayúsculas
+    private $con;
 
-    $con = new mysqli($host, $user, $password, $dbname, $port, $socket);
+    public function __construct() {
+        $this->con = new mysqli($this->host, $this->user, $this->password, $this->dbname, $this->port, $this->socket);
 
-    // Verificar si hay algún error en la conexión
-    if ($con->connect_error) {
-        die("Error al conectar a la base de datos: " . $con->connect_error);
+        // Verificar si hay algún error en la conexión
+        if ($this->con->connect_error) {
+            die("Error al conectar a la base de datos: " . $this->con->connect_error);
+        }
     }
 
-    return $con;
+    public function getConexion() {
+        return $this->con;
+    }
+
+    public function cerrar() {
+        $this->con->close();
+    }
+
+    public function consultar($query) {
+        return $this->con->query($query);
+    }
 }
 ?>
