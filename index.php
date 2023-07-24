5c804cbee3bdd478c7deea1f74ee67a6ae72c1c8
ronalc90 <36252532+ronalc90@users.noreply.github.com>
2023-07-24 09:29:57 -0500

    plantilla

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+
+include 'plantilla.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,21 +10,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <!-- Enlaces a los archivos CSS de Bootstrap -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    
 </head>
 <body>
-    <div class="container mt-4">
-        <div class="row">
-            <div class="col-md-4">
-                <button class="btn btn-primary">Postulación</button>
-            </div>
-            <div class="col-md-4">
-                <button class="btn btn-primary">Proponer Plaza</button>
-            </div>
-            <div class="col-md-4">
-                <button class="btn btn-primary">Mis Postulaciones</button>
-            </div>
-        </div>
+    
         <form action="procesar_busqueda.php" method="get" class="mt-4">
             <div class="form-group">
                 <label for="busqueda">Búsqueda:</label>
@@ -69,9 +63,6 @@
         <button type="submit" class="btn btn-primary">Filtrar</button>
     </div>
 
-    <!-- Enlaces a los archivos JS de Bootstrap si es necesario -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.0.9/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+   
 </body>
 </html>
---
+++ plantilla.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+<div class="container mt-4">
+        <div class="row">
+            <div class="col-md-4">
+                <button class="btn btn-primary">Postulación</button>
+            </div>
+            <div class="col-md-4">
+                <button class="btn btn-primary">Proponer Plaza</button>
+            </div>
+            <div class="col-md-4">
+                <button class="btn btn-primary">Mis Postulaciones</button>
+            </div>
+        </div>
+        <div id="content"> 
+        </div>
+</body>
+</html>
